0994186a362a592764ba9a1a4ed93a9845f7382b
yhf <982435124@qq.com>
2021-06-14 23:23:29 +0800

    删除用户

--- routes/users/index.js
+++ routes/users/index.js
@@ -7,7 +7,6 @@ router.post('/getUser', (req, res) => {
     let { uid } = req.body
     let selPermission = `SELECT * FROM users WHERE uid = ${uid}`
     db.query(selPermission, [uid], (result) => {
-        console.log('输出结果', result)
         let permissionid = result[0].permissionid
         if (parseInt(permissionid) > 2) {
             res.send({
@@ -20,7 +19,6 @@ router.post('/getUser', (req, res) => {
             let selNormalUsers = `SELECT * FROM users, userinfo WHERE permissionid = 3
                                 AND users.uid = userinfo.uid`
             db.query(selNormalUsers, [], (result) => {
-                console.log('输出所有的普通用户', result)
                 res.send({
                     code: 0,
                     data: result
@@ -28,6 +26,31 @@ router.post('/getUser', (req, res) => {
             })
         }
     })
+})
+
+/** 删除用户 */
+router.post('/delUser', (req, res) => {
+    let { uid } = req.body
+    let delsql = `DELETE users, userinfo FROM users LEFT JOIN userinfo ON users.uid = userinfo.uid WHERE users.uid = ${uid}`
+    try {
+        db.query(delsql, [uid], (result) => {
+            console.log(111, result)
+            if (result.affectedRows == 2) {
+                res.send({
+                    code: 0,
+                    msg: '删除成功！'
+                })
+            } else {
+                res.send({
+                    code: -1,
+                    msg: '删除失败！'
+                })
+            }
+        })
+    } catch (error) {
+        
+    }
+    
 })
 
 module.exports = router
